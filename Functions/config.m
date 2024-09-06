9735382e85329b73c4d15b7c9e7e7d32f545cc8e
emmayning <yning7@uic.edu>
2024-09-06 16:22:28 -0500

    Stop tracking config.m and remove it from the repo

--- Functions/config.m
+++
@@ -1,9 +0,0 @@
-
-
-% Add functions to path
-funcPath = '/Users/ningemma/Dropbox/UIC/Prelim/Functions';
-% Add real data & custom functions to path
-dataPath = '/Users/ningemma/Dropbox/UIC/Research/Analyses_2024';
-
-addpath(genpath(funcPath));
-addpath(genpath(dataPath));
